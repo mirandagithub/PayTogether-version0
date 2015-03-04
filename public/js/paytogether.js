3f3f623bf444ee898f4edd5cfaf27dd1a36be670
mirandagithub <miranda.github@gmail.com>
2015-03-04 18:02:35 +0800

    version 0.1
    user login flow

--- public/js/paytogether.js
+++ public/js/paytogether.js
@@ -1,4 +1,143 @@
 Parse.initialize("ZWJYl2molT6LxpiIpgd1yumt2bfOEHzJDq1EjLRr", "c2Xnob8YsgCcvIf38SRUrlcGUiHxdgxb8Skj506T");
- 
+
+
+
+function signupUser(){
+	console.log("sign up user!");        
+	var promise = new Parse.Promise();
+	var currentUser = Parse.User.current();
+	if (currentUser) {
+		console.log("can't sign up , already log in.");
+	} else {
+		var username = $("#signup-username").val();
+    	var password = $("#signup-password").val();
+ 	   	var email = $("#signup-email").val();
+   		var user = new Parse.User();
+   		user.set("username", username);
+    	user.set("password", password);
+    	user.set("email", email);
+    	user.signUp(null, {
+    		success: function(user){
+ 			    console.log(username + " signed up.");
+    			promise.resolve("created new user");
+    		},
+    		error: function(user, error){
+			    console.log(username + " failed signing up.");
+    			promise.reject("can't sign up");
+  	           	$("#error-msg").html("An error occured :( " +"<br>"+ error.message);
+
+    		}
+    	});
+    	console.log(promise);
+    	return promise;
+	}
 
 }
+
+function loginUser(){
+	var promise = new Parse.Promise();
+	var username = $("#login-username").val();
+    var password = $("#login-password").val();
+    console.log([username, password]);
+	Parse.User.logIn(username, password, {
+		success: function(user){
+			promise.resolve("user logged in");
+			console.log("log in user!");        
+			console.log(promise);
+		},
+		error: function(user, error){
+			promise.reject("failed to log in");
+			console.log("failed to log in user!");    
+			console.log(promise);
+  	        $("#error-msg").html("An error occured :( " +"<br>"+ error.message);
+    	}
+	});
+	return promise;
+}
+
+function logoutUser(){
+	var promise = new Parse.Promise();
+	Parse.User.logOut();
+	var user = Parse.User.current();
+	if(user) {
+		promise.reject("can't log out user");
+		console.log("fail to log out user!");   
+  	    $("#error-msg").html("An error occured :( ");
+		console.log(promise);
+	} else {
+		promise.resolve("user has logged out.");
+		console.log("log out user!");   
+		console.log(promise);
+	};
+	
+	return promise;
+}
+
+function initUser(){
+	console.log("Get ready!");       
+    $("#error-msg").html("");
+ 
+	var currentUser = Parse.User.current();
+  	if (currentUser) {
+  		console.log( currentUser.get("username") + " has logged in.");   
+  		// hide log-in page, display log out page
+		$("#msg").html("Good day, " + currentUser.get("username"));
+
+  		$("#userLogin").hide();
+		$("#userSignup").hide();
+
+		$("#data").show();
+		$("#userLogout").show();
+		loadData();
+  } else {
+		console.log("User has not logged in.");        
+		// display log-in page, hide log-out page
+		$("#msg").html("Good day, stranger.");
+		$("#data").hide();
+		$("#userLogout").hide();
+		$("#userLogin").show();
+		$("#userSignup").show();
+ 		cleanData();
+  }
+}
+
+function loadData(user){
+	$("#data-msg").html("will update data");
+}
+
+function cleanData(user){
+	$("#data-msg").html("data goes here");
+}
+
+$(document).ready(function(){
+	$("#signupBtn").click(function(){
+		signupUser().then(function(){
+			//success
+			initUser();
+		}, function(error) {
+			//error goes here
+
+		});
+	})
+
+	$("#loginBtn").click(function(){
+		loginUser().then(function(){
+			//success
+			initUser();
+		}, function(error) {
+			//error goes here
+			
+		});;
+	})
+
+	$("#logoutBtn").click(function(){
+		logoutUser().then(function(){
+			//success
+			initUser();
+		}, function(error) {
+			//error goes here
+			
+		});;;
+	})
+
+}) // end of "document ready" of jQuery
